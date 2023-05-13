e27dfe649833acebce9804b4d43722461e210a17
Andrey <abramov@okap.ru>
2023-05-14 01:44:09 +0300

    fix: tests try 9

--- src/test/java/ru/practicum/shareit/request/RequestIntegrationTests.java
+++ src/test/java/ru/practicum/shareit/request/RequestIntegrationTests.java
@@ -11,8 +11,10 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.exception.EntityNotFoundException;
 import ru.practicum.shareit.exception.InvalidItemParametersException;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.dto.ItemRequestFull;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.request.repository.ItemRequestRepository;
 import ru.practicum.shareit.request.service.ItemRequestService;
@@ -125,14 +127,29 @@ class RequestIntegrationTests {
     @Rollback
     void getItemRequestByIdTest() throws Exception {
         User user = new User(1L, "name","email@mail.ru");
-        ItemRequest itemRequest = new ItemRequest(1L,"description",1L,LocalDateTime.now().minusDays(5));
+        ItemRequestFull itemRequest = new ItemRequestFull(1L,"description",1L,LocalDateTime.now().minusDays(5),null);
+        ItemRequestDto itemRequestDto = new ItemRequestDto(1L,"description",1L,LocalDateTime.now().minusDays(5));
         userRepository.save(user);
-        itemRequestRepository.save(itemRequest);
+        itemRequestService.saveItemRequest(1L,itemRequestDto);
 
         mockMvc.perform(get("/requests/{requestId}", 1L).header("X-Sharer-User-Id", 1L)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(itemRequest))).andExpect(status().isOk())
+                        .andExpect(jsonPath("$.id").value(1L))
                         .andExpect(jsonPath("$.description").value("description"))
                         .andExpect(jsonPath("$.requesterId").value(1L));
     }
+
+    @Test
+    @Rollback
+    void getItemRequestByWrongIdTest() {
+        assertThrows(EntityNotFoundException.class, () -> itemRequestService.getItemRequestById(1L,99L));
+    }
+    @Test
+    @Rollback
+    void getItemRequestByNullIdTest() {
+        assertThrows(EntityNotFoundException.class, () -> itemRequestService.getItemRequestById(1L,null));
+    }
+
+
 }
